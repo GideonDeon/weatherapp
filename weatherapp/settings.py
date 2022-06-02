5fdbd05f307be1fcbd3f98bf503a0fc4346ad67b
gideon <gideong351@gmail.com>
2022-06-02 10:51:29 +0100

    'secure'

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(_vi0yjf4rnd%3_n26h1-it*+w$95ks)@@mvu)$dfqx2@*gb2g'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'app'
 ]
 
